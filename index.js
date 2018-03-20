d3409f5304a19c441f89830488a545d396bc2d01
Isaac McEachin <isaacmceachin@gmail.com>
2018-03-20 02:54:59 -0400

    Most up-to-date changes

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 
+/*{
+	18:19,
+	19:20,
+	20:21,
+	21:22,
+	22:23
+}*/
+
+//Currentlu numbers is blank with no properties on it
+var numbers = {};
+
+//Calling the for loop to start at 18, and continue incrementing until it's reached 22
+for(var iii = 18; iii <= 22; iii++){
+	//within the [] contains the name of a property being added to the numbers object
+	numbers[iii] = (iii+1); //on the right side of the equals the values being stored to that property name
+	
+	//on the first iteration through the loop, iii is set to 18.
+	//on numbers a property of 18 is declared.
+	//the property of 18 on numbers is then set to the value (iii+1) -> 19.
+	//on the next iteration, iii is set to 19, and that property name is stored on numbers as well.
+}
+console.log(numbers);

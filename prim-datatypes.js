b517d1dc9c8faf60f43054d61f38895aa83c363b
Isaac McEachin <isaacmceachin@gmail.com>
2018-03-20 01:50:46 -0400

    Uploading extrapolated js syntax explanation

---
+++ prim-datatypes.js
@@ -0,0 +1,16 @@
+//Primitive Datatypes
+
+//String
+var name = "Isaac";
+
+//Char
+var middle = 'e';
+
+//Integer
+var age = 23;
+
+//Float/Double
+var weight = 186.5;
+
+//boolean
+var alive = true;
